aeb6c1f40521bc415eaf29f8b4dca7c17810a685
shiheiyea <2759688992@qq.com>
2023-11-27 20:57:32 +0800

    订单支付功能代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -20,4 +20,7 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    @Select("select * from user where id = #{userId} ")
+    User getById(Long userId);
 }
