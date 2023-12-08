c4613f193667b8dbfd139d25837c0fff87b35622
shiheiyea <2759688992@qq.com>
2023-12-08 21:13:37 +0800

    导出运营数据报表

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -91,5 +93,16 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end) {
         log.info("销量排名: {}, {}", begin, end);
         return Result.success(reportService.getSalesTop10(begin, end));
+    }
+
+    /**
+     * 导出运用数据报表
+     *
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运用数据报表")
+    public void export(HttpServletResponse response) {
+        reportService.exportBusinessData(response);
     }
 }
--- sky-server/src/main/java/com/sky/service/ReportService.java
+++ sky-server/src/main/java/com/sky/service/ReportService.java
@@ -5,25 +5,29 @@ import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 public interface ReportService {
 
     /**
      * 统计指定区间类的营业额数据
+     *
      * @param begin
      * @param end
      * @return
      */
-    TurnoverReportVO getTurnoverStatistics (LocalDate begin, LocalDate end);
+    TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end);
 
     /**
      * 统计指定区间类的用户数据
+     *
      * @param begin
      * @param end
      * @return
      */
-    UserReportVO getUserStatistics (LocalDate begin, LocalDate end);
+    UserReportVO getUserStatistics(LocalDate begin, LocalDate end);
 
     /**
      * 订单统计
@@ -36,9 +40,17 @@ public interface ReportService {
 
     /**
      * 统计指定时间区间的销量排名前10
+     *
      * @param begin
      * @param end
      * @return
      */
     SalesTop10ReportVO getSalesTop10(LocalDate begin, LocalDate end);
+
+    /**
+     * 导出运营数据报表
+     *
+     * @param response
+     */
+    void exportBusinessData(HttpServletResponse response);
 }
